621ad8fb0b9b7958b4aa892c197664eba300b7f2
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-04 10:00:18 -0500

    Adjusted

--- Cass Attempt 3/Cass Attempt 3.cpp
+++ Cass Attempt 3/Cass Attempt 3.cpp
@@ -6,6 +6,6 @@ int main()
 {
     int age = 18;
     double weight = 160;
-    cout << 'My age is' << age << '' and my weight is ' << weight << endl;
+    cout << "My age is" << age << " and my weight is" << weight << endl;
     return 0;
 }
