2761cb266b0b893846924e7f66a51becf4fa51b0
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-04 09:59:18 -0500

    Added code for my weight and age

--- Cass Attempt 3/Cass Attempt 3.cpp
+++ Cass Attempt 3/Cass Attempt 3.cpp
@@ -1,8 +1,11 @@
 
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Bonjour Tout Le Monde!\n";
+    int age = 18;
+    double weight = 160;
+    cout << 'My age is' << age << '' and my weight is ' << weight << endl;
+    return 0;
 }
