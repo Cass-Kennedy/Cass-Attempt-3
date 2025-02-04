88d7797c5bb64709e5562a6310e961b040f9d211
Cass-Kennedy <alkennedy228@gmail.com>
2025-02-04 09:55:23 -0500

    Added code to display my name, address, and phone number.

--- Cass Attempt 3/Cass Attempt 3.cpp
+++ Cass Attempt 3/Cass Attempt 3.cpp
@@ -1,8 +1,15 @@
 
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Bonjour Tout Le Monde!\n";
+    string name, address, phone;
+    name = "Cass Kennedy";
+    address = "555 Elderwood Road";
+    phone = "757-555-3261";
+    cout << name << endl;
+    cout << address << endl;
+    cout << phone << endl;
+    return 0;
 }
